41a2f49e2e995adc74eec2bb45af1382e6a3ae5d
Roman Dvornov <rdvornov@gmail.com>
2020-08-25 00:00:42 +0200

    Tweak info test suite

--- test/info.js
+++ test/info.js
@@ -1,8 +1,7 @@
 const assert = require('assert');
 const { inspect } = require('util');
 const { info: jsonStringifyInfo } = require('../src');
-const stringEncoder = new TextEncoder();
-const strBytesLength = str => stringEncoder.encode(str).length;
+const strBytesLength = str => Buffer.byteLength(str, 'utf8');
 
 describe('info()', () => {
     describe('basic', () => {
@@ -12,26 +11,31 @@ describe('info()', () => {
             false,
             123,
             -123,
+            // 5
             NaN,
             Infinity,
             -Infinity,
             'test',
             '\b\t\n\f\r"\\', // escapes
+            // 10
             '漢字',
             '\u0000\u0010\u001f\u009f', // "\u009f"
             '\uD800\uDC00',  // surrogate pair
             '\uDC00\uD800',  // broken surrogate pair
             '\uD800',  // leading surrogate (broken surrogate pair)
+            // 15
             '\uDC00',  // trailing surrogate (broken surrogate pair)
             Array.from({ length: 0x900 }).map((_, i) => String.fromCharCode(i)).join(''), // all chars 0x00..0x8FF
             {},
             { foo: 1 },
             { foo: 1, bar: 2 },
+            // 20
             { foo: 1, bar: undefined, baz: { a: undefined, b: 123, c: [1, 2] } },
             { foo: 1, bar: NaN, baz: Infinity, qux: -Infinity },
             [],
             [1, 2, 3],
             [{ foo: 1 }, undefined, 123, NaN, Infinity, -Infinity, 'test'],
+            // 25
             undefined,
             Symbol('test'),
             { foo: 1, bar: Symbol('test') },
@@ -41,7 +45,6 @@ describe('info()', () => {
 
         describe('no spaces', () => {
             for (const value of tests) {
-                jsonStringifyInfo(value);
                 it(inspect(value, { depth: null }), () => {
                     const native = String(JSON.stringify(value));
                     const info = jsonStringifyInfo(value);
