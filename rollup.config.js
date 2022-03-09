61b6f8b2fa787bf37257c74f60a23cd123e632da
Roman Dvornov <rdvornov@gmail.com>
2022-03-09 18:27:19 +0100

    Fix write dist/version.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,9 @@
 const fs = require('fs');
-const path = require('path');
 const { nodeResolve } = require('@rollup/plugin-node-resolve');
 const commonjs = require('@rollup/plugin-commonjs');
 const json = require('@rollup/plugin-json');
 const { terser } = require('rollup-plugin-terser');
-const versionFile = path.join(__dirname, 'src/version.js');
+let versionWritten = false;
 
 module.exports = {
     input: 'src/index.js',
@@ -13,8 +12,10 @@ module.exports = {
         { name: 'jsonExt', format: 'umd', file: 'dist/json-ext.min.js', plugins: [terser({ compress: { passes: 2 } })] }
     ],
     plugins: [
-        { name: 'version', resolveId(id) {
-            if (id === versionFile) {
+        { name: 'version', resolveId() {
+            if (!versionWritten) {
+                versionWritten = true;
+                console.log('Write ./dist/version.js');
                 fs.writeFileSync('./dist/version.js', `module.exports = "${require('./package.json').version}";`);
             }
         } },
