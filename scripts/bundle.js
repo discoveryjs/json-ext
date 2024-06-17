37b7100e6a89e17f31451b6fb427c48977dcf9c7
Roman Dvornov <rdvornov@gmail.com>
2024-06-17 18:35:09 +0200

    Rework transpile and bundle

---
+++ scripts/bundle.js
@@ -0,0 +1,55 @@
+import fs from 'fs';
+import esbuild from 'esbuild';
+
+const log = async (outfile, fn) => {
+    const start = Date.now();
+    try {
+        await fn(outfile);
+    } finally {
+        const stat = fs.statSync(outfile);
+        if (stat.isDirectory()) {
+            console.log(outfile, 'in', Date.now() - start, 'ms');
+        } else {
+            console.log(outfile, stat.size, 'bytes in', Date.now() - start, 'ms');
+        }
+    }
+};
+
+const banner = { js: `(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global.jsonExt = factory());
+}(typeof globalThis != 'undefined' ? globalThis : typeof window != 'undefined' ? window : typeof global != 'undefined' ? global : typeof self != 'undefined' ? self : this, (function () {` };
+const footer = { js: `
+  return exports;
+})));` };
+
+async function build() {
+    const commonOptions = {
+        entryPoints: ['src/index.js'],
+        target: ['es2020'],
+        format: 'iife',
+        globalName: 'exports',
+        // write: false,
+        banner,
+        footer,
+        bundle: true
+    };
+
+    // bundle
+    await log('dist/json-ext.js', (outfile) => esbuild.build({
+        ...commonOptions,
+        // write: false,
+        outfile
+    }));
+
+    // minified bundle
+    await log('dist/json-ext.min.js', (outfile) => esbuild.build({
+        ...commonOptions,
+        // write: false,
+        outfile,
+        minify: true
+    }));
+}
+
+build();
---
+++ scripts/transpile.cjs
@@ -0,0 +1,135 @@
+const fs = require('fs');
+const path = require('path');
+const { rollup, watch } = require('rollup');
+const chalk = require('chalk');
+
+const external = [
+    'node:fs',
+    'node:url',
+    'node:path',
+    'node:assert',
+    'node:stream',
+    'node:util'
+];
+
+function resolvePath(ts = false, ext) {
+    return {
+        name: 'transpile-ts',
+        resolveId(source, parent) {
+            if (parent && !/\/(src|lib)\//.test(parent) && /\/(src|lib)\//.test(source)) {
+                return {
+                    id: source
+                        // .replace(/\/lib\//, '/cjs/')
+                        .replace(/\/src\//, '/cjs/')
+                        .replace(/\.js$/, ext),
+                    external: true
+                };
+            }
+            if (ts && parent && source.startsWith('.')) {
+                const resolved = path.resolve(path.dirname(parent), source);
+                const resolvedTs = resolved.replace(/.js$/, '.ts');
+
+                return fs.existsSync(resolvedTs) ? resolvedTs : resolved;
+            }
+            return null;
+        }
+    };
+}
+
+function readDir(dir) {
+    return fs
+        .readdirSync(dir)
+        .filter((fn) => fn.endsWith('.js') || fn.endsWith('.ts'))
+        .map((fn) => `${dir}/${fn}`);
+}
+
+async function transpile({
+    entryPoints,
+    outputDir,
+    format,
+    watch: watchMode = false,
+    ts = false,
+    onSuccess
+}) {
+    const outputExt = format === 'esm' ? '.js' : '.cjs';
+    const doneMessage = (duration) =>
+        `${
+            ts ? 'Compile TypeScript to JavaScript (ESM)' : 'Convert ESM to CommonJS'
+        } into "${outputDir}" done in ${duration}ms`;
+
+    const inputOptions = {
+        external,
+        input: entryPoints,
+        plugins: [
+            resolvePath(ts, outputExt)
+        ]
+    };
+    const outputOptions = {
+        dir: outputDir,
+        entryFileNames: `[name]${outputExt}`,
+        sourcemap: ts,
+        format,
+        exports: 'auto',
+        preserveModules: true,
+        interop: false,
+        esModule: format === 'esm',
+        generatedCode: {
+            constBindings: true
+        }
+    };
+
+    if (!watchMode) {
+        const startTime = Date.now();
+        const bundle = await rollup(inputOptions);
+        await bundle.write(outputOptions);
+        await bundle.close();
+
+        console.log(doneMessage(Date.now() - startTime));
+
+        if (typeof onSuccess === 'function') {
+            await onSuccess();
+        }
+    } else {
+        const watcher = watch({
+            ...inputOptions,
+            output: outputOptions
+        });
+
+        watcher.on('event', ({ code, duration, error }) => {
+            if (code === 'BUNDLE_END') {
+                console.log(doneMessage(duration));
+
+                if (typeof onSuccess === 'function') {
+                    onSuccess();
+                }
+            } else if (code === 'ERROR') {
+                console.error(chalk.bgRed.white('ERROR!'), chalk.red(error.message));
+            }
+        });
+    }
+}
+
+async function transpileAll(options) {
+    const { watch = false } = options || {};
+
+    await transpile({
+        entryPoints: ['src/index.js'],
+        outputDir: './cjs',
+        format: 'cjs',
+        watch
+    });
+    await transpile({
+        entryPoints: readDir('test'),
+        outputDir: './cjs-test',
+        format: 'cjs',
+        watch
+    });
+}
+
+module.exports = transpileAll;
+
+if (require.main === module) {
+    transpileAll({
+        watch: process.argv.includes('--watch')
+    });
+}
